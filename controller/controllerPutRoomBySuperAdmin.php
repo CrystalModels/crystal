4b2e37b7b0d5b32fe828e029f6996e34dd7193de
Juan <juan.paez@api-ux.com>
2023-07-06 20:41:27 -0500

    Nueva funcionalidad

--- controller/controllerPutRoomBySuperAdmin.php
+++ controller/controllerPutRoomBySuperAdmin.php
@@ -28,7 +28,7 @@ $curl = curl_init();
 // Configurar las opciones de la sesión cURL
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $json_data);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
 // Agregar los headers
@@ -47,13 +47,13 @@ curl_close($curl);
 
 $response1 = trim($response1); // Eliminar espacios en blanco alrededor de la respuesta
 
-//echo $_SESSION['key'];
+echo $response1;
 if (strtolower($response1) === "true") { // Convertir la respuesta a minúsculas antes de comparar
     $_SESSION["respuesta"] = "true";
     $_SESSION["mensaje"] = "Room editado correctamente";
     $_SESSION["error"] = $response1;
     
- 
+    echo $response1;
   
     header ('Location: ../room.php?roomId='.$roomId);
 }
@@ -65,7 +65,7 @@ if (strtolower($response1) === "false") { // Convertir la respuesta a minúscula
     
   
     
-  
+    echo $response1;
     header ('Location: ../room.php?roomId='.$roomId);
 }
 ?>
