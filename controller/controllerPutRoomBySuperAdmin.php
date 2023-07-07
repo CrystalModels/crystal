3f148860f8c9781505e7f8129d33ae3c1434b964
Juan <juan.paez@api-ux.com>
2023-07-06 20:49:38 -0500

    Nueva funcionalidad

--- controller/controllerPutRoomBySuperAdmin.php
+++ controller/controllerPutRoomBySuperAdmin.php
@@ -9,7 +9,7 @@ require_once '../env/domain.php';
 $sub_domaincon = new model_dom();
 $sub_domain = $sub_domaincon->dom();
 
-$url = '' . $sub_domain . '/crystalGateway/apiIntegrations/v1/putRooms/';
+$url = $sub_domain . "/crystalGateway/apiIntegrations/v1/putRooms/".$_SESSION['ranCode']."/".$_SESSION['key'];
 
 // Definir los datos a enviar en la solicitud POST
 $data = array(
@@ -30,14 +30,6 @@ curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-// Agregar los headers
-$headers = array(
-    'Content-Type: application/json', // Tipo de contenido JSON
-    'Api-Key: '.$_SESSION['ranCode'], // Token de autorización
-    'x-api-Key: '.$_SESSION['key']
-);
-curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
 // Ejecutar la solicitud y obtener la respuesta
 $response1 = curl_exec($curl);
