350e5543f56898ac8e5e2acb8a93c936bff9955b
Juan <juan.paez@api-ux.com>
2023-07-07 11:18:40 -0500

    Nueva funcionalidad

--- controller/controllerPutRoomStatusBySuperAdmin.php
+++ controller/controllerPutRoomStatusBySuperAdmin.php
@@ -1,19 +1,22 @@
 <?php
 
 session_start();
-$status = $_POST['status'];
+$name = $_POST['name'];
+$comments = $_POST['comments'];
 $roomId = $_GET['roomId'];
 
 require_once '../env/domain.php';
 $sub_domaincon = new model_dom();
 $sub_domain = $sub_domaincon->dom();
 
-$url = '' . $sub_domain . '/crystalGateway/apiIntegrations/v1/putRoomsStatus/';
+$url = $sub_domain . "/crystalGateway/apiIntegrations/v1/putRoomsStatus/".$_SESSION['ranCode']."/".$_SESSION['key'];
 
 // Definir los datos a enviar en la solicitud POST
 $data = array(
-    'value' => $status, 
+    
+    'value' => $value,
     'roomId' => $roomId
+    
 );
 
 // Convertir los datos a formato JSON
@@ -25,16 +28,8 @@ $curl = curl_init();
 // Configurar las opciones de la sesión cURL
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $json_data);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-// Agregar los headers
-$headers = array(
-    'Content-Type: application/json', // Tipo de contenido JSON
-    'Api-Key: '.$_SESSION['ranCode'], // Token de autorización
-    'x-api-Key: '.$_SESSION['key']
-);
-curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
 // Ejecutar la solicitud y obtener la respuesta
 $response1 = curl_exec($curl);
@@ -43,25 +38,26 @@ $response1 = curl_exec($curl);
 curl_close($curl);
 
 $response1 = trim($response1); // Eliminar espacios en blanco alrededor de la respuesta
-echo $response1;
-//echo $_SESSION['key'];
+
+//echo $response1;
 if (strtolower($response1) === "true") { // Convertir la respuesta a minúsculas antes de comparar
     $_SESSION["respuesta"] = "true";
     $_SESSION["mensaje"] = "Estado editado correctamente";
     $_SESSION["error"] = $response1;
     
- 
+
     
     header ('Location: ../room.php?roomId='.$roomId);
 }
 
-if (strtolower($response1) === "false") { // Convertir la respuesta a minúsculas antes de comparar
+if (strtolower($response1) != "true") { // Convertir la respuesta a minúsculas antes de comparar
     $_SESSION["respuesta"] = "false";
     $_SESSION["mensaje"] = "Error al editar estado";
     $_SESSION["error"] = $response1;
     
   
+  
+  
     header ('Location: ../room.php?roomId='.$roomId);
-
 }
 ?>
