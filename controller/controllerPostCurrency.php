5dbece7a9a91b874466029c9a185869c43c8415a
Juan <juan.paez@api-ux.com>
2023-07-08 13:30:32 -0500

    Nueva funcionalidad

--- controller/controllerPostCurrency.php
+++ controller/controllerPostCurrency.php
@@ -11,7 +11,7 @@ require_once '../env/domain.php';
 $sub_domaincon = new model_dom();
 $sub_domain = $sub_domaincon->dom();
 
-$url = '' . $sub_domain . '/crystalGateway/apiIntegrations/v1/postCurrency/';
+$url = $sub_domain . "/crystalGateway/apiIntegrations/v1/postCurrency/".$_SESSION['ranCode']."/".$_SESSION['key'];
 
 // Definir los datos a enviar en la solicitud POST
 $data = array(
@@ -31,16 +31,10 @@ $curl = curl_init();
 // Configurar las opciones de la sesión cURL
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $json_data);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-// Agregar los headers
-$headers = array(
-    'Content-Type: application/json', // Tipo de contenido JSON
-    'Api-Key: '.$_SESSION['ranCode'], // Token de autorización
-    'x-api-Key: '.$_SESSION['key']
-);
-curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
 
 // Ejecutar la solicitud y obtener la respuesta
 $response1 = curl_exec($curl);
@@ -49,7 +43,7 @@ $response1 = curl_exec($curl);
 curl_close($curl);
 
 $response1 = trim($response1); // Eliminar espacios en blanco alrededor de la respuesta
-echo $response1;
+
 //echo $_SESSION['key'];
 if (strtolower($response1) === "true") { // Convertir la respuesta a minúsculas antes de comparar
     $_SESSION["respuesta"] = "true";
