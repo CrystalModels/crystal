cc08fd0ad19595d81e3ae51d40192c5f982b7204
Juan <juan.paez@api-ux.com>
2023-07-09 13:38:37 -0500

    Nueva funcionalidad

--- controller/controllerValidateFirstStepChangePass.php
+++ controller/controllerValidateFirstStepChangePass.php
@@ -30,7 +30,7 @@ $curl = curl_init();
 // Configurar las opciones de la sesión cURL
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $json_data);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
 // Agregar los headers
@@ -43,9 +43,7 @@ $response1 = curl_exec($curl);
 // Cerrar la sesión cURL
 curl_close($curl);
 
-$response2 = trim($response1); // Eliminar espacios en blanco alrededor de la respuesta
-$parts = explode(" ", $response2);
-$response1=$parts[0];
+$response1 = trim($response1);
 //echo $_SESSION['key'];
 if (strtolower($response1) === "true") { // Convertir la respuesta a minúsculas antes de comparar
    
