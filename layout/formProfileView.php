e9e351d966d23b5d6782531fa875a9bdcc35121c
Juan <juan.paez@api-ux.com>
2023-07-08 21:41:28 -0500

    Nueva funcionalidad

--- layout/formProfileView.php
+++ layout/formProfileView.php
@@ -82,7 +82,7 @@ echo '
         <script>
         //const rancode = sessionStorage.getItem("ranCode");
         //const key = sessionStorage.getItem("key");
-        const urlMyProfile = "' . $sub_domain . '/crystalGateway/apiCore/v1/getProfileInfoLogJS/'.$_SESSION['ranCode'].'/'.$_SESSION['key'].'/'.$_GET['userName'].'";
+        const urlMyProfile = "' . $sub_domain . '/crystalGateway/apiCore/v1/getProfileInfoLogJS/'.$_SESSION['ranCode'].'/'.$_SESSION['key'].'/'.$_GET['userName'].'/'.$_SESSION['sessionId'].'";
         </script>';
         echo '<script>
             // Función para obtener los datos del API
