cd45c5455c6cc6dbaf71ab90d76d8a1b3cd0e6be
Juan <juan.paez@api-ux.com>
2023-07-25 18:29:15 -0500

    homologacion modales textos v1.0.2-beta

--- layout/tableModels.php
+++ layout/tableModels.php
@@ -46,7 +46,7 @@ $headerslink=$_SESSION['ranCode']." ".$_SESSION['key'];
         <td>${student.lastName}</td>
         <td>${student.rol}</td>
 
-        <td><button onclick="openModalPagesAssign();getPagesAssign(&quot;${student.profileId}&quot;);">Asigna Página</button><button onclick="openModalRoomsModelassign();getRoomsAssign(&quot;${student.profileId}&quot;);">Asignar room</button></td>
+        <td><button onclick="openModalPagesAssign();getPagesAssign(&quot;${student.profileId}&quot;);" class="table-button">Asigna Página</button><button onclick="openModalRoomsModelassign();getRoomsAssign(&quot;${student.profileId}&quot;);">Asignar room</button></td>
         <td><button onclick="openModalPagesModelHis();getPagesAssignModelsHis1(&quot;${student.profileId}&quot;,&quot;${student.profileId}&quot;);">Historial de transmisión</button></td>
       
       
