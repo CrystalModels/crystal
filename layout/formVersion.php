c179700bf408825bcb44cbd04f5fba8c7f7b02b7
Juan <juan.paez@api-ux.com>
2023-07-26 11:14:17 -0500

    homologacion footer v1.0.4-beta

---
+++ layout/formVersion.php
@@ -0,0 +1,36 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/x-icon" href="public/logo.png">
+  <title>Iniciar - Crystal Models</title>
+  <link rel="stylesheet" href="style/styleSession.css">
+
+
+</head>
+<body>
+    
+
+ 
+    <h2><img src="public/logo.png" alt="Crystal Models"></h2>
+    <form method="post" action="controller/controllerChangePassSession.php">
+
+    <div class="form-group">
+
+
+    <div class="form-group">
+    <label for="nkeyword">Recuerda que la contraseña debe contener mínimo 8 carácteres (MAYúSCULAS, minúsculas, simbolos (*.-_?¡¿!) y números)</label>
+    </div> 
+
+    
+       </form>
+
+ 
+
+ 
+</body>
+</html>
