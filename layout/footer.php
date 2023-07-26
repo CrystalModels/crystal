4ecdb3388e031d2637d8176db2f16ee72204e5e9
Juan <juan.paez@api-ux.com>
2023-07-26 11:00:01 -0500

    homologacion footer v1.0.4-beta

--- layout/footer.php
+++ layout/footer.php
@@ -22,12 +22,8 @@
       ?></p>
             
             <p>Soporte: soporte@example.com</p>
-            <p><a href="#" onclick="openModalPrevVer();">Versiones anteriores</a></p>
+            <p><a href="room.php?roomId=${student.roomId}" target="_blank">Versiones anteriores</a></p>
             <p>&copy; <?php 
     echo $ano_actual = date('Y');?></p>
           </div>
     </footer>
-
-    <?php
-    require_once 'modal/modalPrevVer.php';
-    ?>
