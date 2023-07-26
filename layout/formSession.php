d2d80813d4d35f848172dda966d389fce53d0c4a
Juan <juan.paez@api-ux.com>
2023-07-26 09:38:41 -0500

    homologacion modales textos v1.0.2-beta

--- layout/formSession.php
+++ layout/formSession.php
@@ -108,6 +108,16 @@
       chart.draw(data, options);
     }
   </script>
+
+
+
+<footer>
+        <div>
+            <p>Versión del software: 1.0.0</p>
+            <p>&copy; 2023 Todos los derechos reservados</p>
+            <p>Contacto de soporte: soporte@example.com</p>
+        </div>
+    </footer>
 </body>
 </html>
 
