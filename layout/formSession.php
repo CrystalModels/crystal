d3d94c7973381ae18e1dc9c66652f99a3ad01107
Juan <juan.paez@api-ux.com>
2023-07-26 10:08:30 -0500

    homologacion modales textos v1.0.2-beta

--- layout/formSession.php
+++ layout/formSession.php
@@ -69,11 +69,12 @@
       $sub_domaincon = new model_ver();
       echo $sub_domain = $sub_domaincon->versioning();
       ?></p>
-            <p>&copy; <?php 
-    echo $ano_actual = date('Y');?></p>
+            
             <p>Soporte: soporte@example.com</p>
             <p><a href="room.php?roomId=${student.roomId}" target="_blank">Versiones anteriores</a></p>
-        </div>
+            <p>&copy; <?php 
+    echo $ano_actual = date('Y');?></p>
+          </div>
     </footer>
 </html>
 
