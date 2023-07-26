f0b157b9303e5b2060243c66edf95a7128b9541b
Juan <juan.paez@api-ux.com>
2023-07-26 10:10:44 -0500

    homologacion modales textos v1.0.2-beta

--- layout/footer.php
+++ layout/footer.php
@@ -1,7 +1,29 @@
+
+
+<style>
+          footer {
+            background-color: #f0f0f0;
+            padding: 5px;
+            text-align: center;
+            font-size: 10px;
+            color: #555;
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+        }
+    </style>
 <footer>
         <div>
-            <p>Versión del software: 1.0.0</p>
-            <p>&copy; 2023 Todos los derechos reservados</p>
-            <p>Contacto de soporte: soporte@example.com</p>
-        </div>
+            <p>Versión del software: <?php
+      require_once 'version/version.php';
+      $sub_domaincon = new model_ver();
+      echo $sub_domain = $sub_domaincon->versioning();
+      ?></p>
+            
+            <p>Soporte: soporte@example.com</p>
+            <p><a href="room.php?roomId=${student.roomId}" target="_blank">Versiones anteriores</a></p>
+            <p>&copy; <?php 
+    echo $ano_actual = date('Y');?></p>
+          </div>
     </footer>
--- layout/formSession.php
+++ layout/formSession.php
@@ -46,36 +46,7 @@
 </body>
 
 
-
-
-
-<style>
-          footer {
-            background-color: #f0f0f0;
-            padding: 5px;
-            text-align: center;
-            font-size: 10px;
-            color: #555;
-            position: fixed;
-            bottom: 0;
-            left: 0;
-            width: 100%;
-        }
-    </style>
-<footer>
-        <div>
-            <p>Versión del software: <?php
-      require_once 'version/version.php';
-      $sub_domaincon = new model_ver();
-      echo $sub_domain = $sub_domaincon->versioning();
-      ?></p>
-            
-            <p>Soporte: soporte@example.com</p>
-            <p><a href="room.php?roomId=${student.roomId}" target="_blank">Versiones anteriores</a></p>
-            <p>&copy; <?php 
-    echo $ano_actual = date('Y');?></p>
-          </div>
-    </footer>
+     <?php require_once 'footer.php';?>
 </html>
 
 
