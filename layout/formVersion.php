0c4e991ac97254e99c7591f7ac2e2912a35cffcf
Juan <juan.paez@api-ux.com>
2023-07-26 14:08:56 -0500

    visualizador de versiones v1.0.5-beta

--- layout/formVersion.php
+++ layout/formVersion.php
@@ -52,23 +52,14 @@
 
  
 
- 
-</body>
-</html>
 
 
 
 
 
 
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   
-</head>
-<body>
+
+
     <h2>CORE</h2>
     <table id="tabla-versions">
         <thead>
