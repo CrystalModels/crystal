c86d73418642f2652b2a96a66a8926de6febc90b
Juan <juan.paez@api-ux.com>
2023-07-26 14:05:45 -0500

    visualizador de versiones v1.0.5-beta

--- layout/formVersion.php
+++ layout/formVersion.php
@@ -55,3 +55,73 @@
  
 </body>
 </html>
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+</head>
+<body>
+    <h1>Tabla de Versiones</h1>
+    <table id="tabla-versions">
+        <thead>
+            <tr>
+                <th>Versión</th>
+                <th>Contenido</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- La tabla se llenará dinámicamente aquí -->
+        </tbody>
+    </table>
+    <?php
+
+require_once 'env/domain.php';
+$sub_domaincon=new model_dom;
+$sub_domain=$sub_domaincon->dom();
+$headerslink=$_SESSION['ranCode']." ".$_SESSION['key'];
+	echo '
+    <script>
+        // URL del API
+        const apiUrl = `'.$sub_domain.'/crystalGateway/apiBroker/v1/getVersionListCoreApiUsers/'.$headerslink.'`;
+
+        // Obtener la referencia a la tabla
+        const tablaVersions = document.getElementById("tabla-versions").getElementsByTagName("tbody")[0];
+
+        // Función para llenar la tabla con los datos del API
+        async function llenarTabla() {
+            try {
+                // Realizar la solicitud al API
+                const response = await fetch(apiUrl);
+                const data = await response.json();
+
+                // Iterar sobre los datos y llenar la tabla
+                for (const entry of data["crystalCore-apiUsers"]) {
+                    for (const version in entry) {
+                        const fecha = entry[version];
+
+                        const row = tablaVersions.insertRow();
+                        const cellVersion = row.insertCell();
+                        const cellFecha = row.insertCell();
+
+                        cellVersion.textContent = version;
+                        cellFecha.textContent = fecha;
+                    }
+                }
+            } catch (error) {
+                console.error("Error al obtener los datos del API:", error);
+            }
+        }
+
+        // Llamar a la función para llenar la tabla
+        llenarTabla();
+    </script>';?>
+</body>
+</html>
