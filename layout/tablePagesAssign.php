45951cd70eb0f8954852ae652d580ee7bf470bca
Juan <juan.paez@api-ux.com>
2023-08-04 11:09:56 -0500

    calculos por transmision v1.3.0-beta

--- layout/tablePagesAssign.php
+++ layout/tablePagesAssign.php
@@ -118,7 +118,7 @@ function asignarPagina(button, pageid, profileid) {
       var res='<?php $_SESSION['mensaje'];?>'
       sessionStorage.setItem('mensaje',res);
       var mensaje = sessionStorage.getItem("mensaje");
-      mostrarNotificacion(mensaje,'success');
+      mostrarNotificacion(res,'success');
       
  
     })
