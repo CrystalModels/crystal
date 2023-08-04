697d820624c1b3b489a76372b819f2d97e4a92d2
Juan <juan.paez@api-ux.com>
2023-08-04 11:03:03 -0500

    calculos por transmision v1.3.0-beta

--- layout/tablePagesAssign.php
+++ layout/tablePagesAssign.php
@@ -116,7 +116,7 @@ function asignarPagina(button, pageid, profileid) {
       var res='<?php $_SESSION['mensaje'];?>'
       sessionStorage.setItem('mensaje',res);
       var mensaje = sessionStorage.getItem("mensaje");
-      showAlert(res);
+      mostrarNotificacion('exito','success');
       
  
     })
@@ -125,94 +125,51 @@ function asignarPagina(button, pageid, profileid) {
       console.log('Error en la petición:', error);
     });
 }
+
+
+
+
+
+
+
+
+
+
+// Función para mostrar la notificación
+function mostrarNotificacion(mensaje, tipo) {
+    const notificacion = document.createElement('div');
+    notificacion.classList.add('notification');
+    if (tipo === 'error') {
+        notificacion.classList.add('error');
+    }
+    notificacion.textContent = mensaje;
+    document.body.appendChild(notificacion);
+    
+    // Desaparecer la notificación después de 3 segundos
+    setTimeout(() => {
+        notificacion.style.display = 'none';
+    }, 3000);
+}
+
 </script>
 
 <style>
-		.alert {
-			position: fixed;
-			top: -100px;
-			width: 100%;
-			background-color: #2d572c;
-			color: white;
-			text-align: center;
-			padding: 3px;
-			transition: top 0.6s ease;
-			z-index: 2;
-		}
+		/* Estilos para la notificación */
+.notification {
+    position: fixed;
+    top: 20px;
+    right: 20px;
+    background-color: #4CAF50;
+    color: #fff;
+    padding: 10px;
+    border-radius: 5px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+    display: none;
+}
+.notification.error {
+    background-color: #f44336;
+}
 
-		.alertno {
-			position: fixed;
-			top: -100px;
-			width: 100%;
-			background-color: #cb3234;
-			color: white;
-			text-align: center;
-			padding: 3px;
-			transition: top 0.6s ease;
-			z-index: 2;
-		}
 	</style>
-  <div id="alert" class="alert"></div>
-	<div id="alertno" class="alertno"></div>
   
 
-
-    
-<?php
-  
-  echo '<script>
-  const respuesta = "' . $_SESSION["respuesta"] . '";
-  sessionStorage.setItem("respuesta", respuesta);
-  const mensaje = "' . $_SESSION["mensaje"] . '";
-  sessionStorage.setItem("mensaje", mensaje);
-  const error = "' . $_SESSION["error"] . '";
-  sessionStorage.setItem("error", error);
-
-
-
-
-
-  const alerta = sessionStorage.getItem("respuesta");
-  if(alerta=="true"){
-	const mensaje = sessionStorage.getItem("mensaje");
-	  showAlert(mensaje);
-  }
-  if(alerta=="false"){
-	const mensaje = sessionStorage.getItem("mensaje");
-	  showAlertno(mensaje);
-  }
-
-		function showAlert(alertas) {
-    // Mostrar la alerta
-    var alert = document.getElementById("alert");
-    alert.innerHTML = alertas;
-    alert.style.top = "o";
-
-    // Ocultar la alerta después de 5 segundos
-    setTimeout(function(){
-        alert.style.top = "-100px";
-    }, 5000);
-	
-	//sessionStorage.removeItem("respuesta");
-	//sessionStorage.removeItem("mensaje");
-}
-
-function showAlertno(alertas) {
-    // Mostrar la alerta
-    var alertno = document.getElementById("alertno");
-    alertno.innerHTML = alertas;
-    alertno.style.top = "0";
-
-    // Ocultar la alerta después de 5 segundos
-    setTimeout(function(){
-        alertno.style.top = "-100px";
-    }, 5000);
-	
-
-}
-
-  
-</script>';
-
-//echo $_SESSION['key'];
-?>
