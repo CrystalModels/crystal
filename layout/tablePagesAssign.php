54c65572cd2e5960758affe95d186f3343335316
Juan <juan.paez@api-ux.com>
2023-08-04 13:53:32 -0500

    calculos por transmision v1.3.0-beta

--- layout/tablePagesAssign.php
+++ layout/tablePagesAssign.php
@@ -134,29 +134,6 @@ function asignarPagina(button, pageid, profileid) {
 
 
 
-// Función para mostrar la notificación
-function mostrarNotificacion(mensaje, tipo) {
-    const notificacion = document.getElementById('notification');
-    const notificacionText = document.getElementById('notificationText');
-
-    notificacionText.textContent = mensaje;
-    notificacion.classList.remove('error'); // Remover clase de error (en caso de que esté presente)
-
-    if (tipo === 'error') {
-        notificacion.classList.add('error');
-    }
-
-    notificacion.style.display = 'block';
-
-    // Desaparecer la notificación después de 3 segundos
-    setTimeout(() => {
-        notificacion.style.display = 'none';
-    }, 3000);
-}
-
-
-
-
 
 
 
--- session.php
+++ session.php
@@ -32,3 +32,32 @@ if($_SESSION['rol']=="PHOTO"){
 <?php 
  
 ?>
+
+<script>
+
+
+    
+
+// Función para mostrar la notificación
+function mostrarNotificacion(mensaje, tipo) {
+    const notificacion = document.getElementById('notification');
+    const notificacionText = document.getElementById('notificationText');
+
+    notificacionText.textContent = mensaje;
+    notificacion.classList.remove('error'); // Remover clase de error (en caso de que esté presente)
+
+    if (tipo === 'error') {
+        notificacion.classList.add('error');
+    }
+
+    notificacion.style.display = 'block';
+
+    // Desaparecer la notificación después de 3 segundos
+    setTimeout(() => {
+        notificacion.style.display = 'none';
+    }, 3000);
+}
+
+
+
+</script>
