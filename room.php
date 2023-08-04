b911ed68669435f54aaf4e09940caea80114f11e
Juan <juan.paez@api-ux.com>
2023-08-04 14:21:12 -0500

    calculos por transmision v1.3.0-beta

--- room.php
+++ room.php
@@ -3,3 +3,87 @@
 require_once 'view/viewRoom.php';
 
 ?>
+
+<div class="notification" id="notification">
+        <p id="notificationText"></p>
+    </div>
+<script>
+
+
+    
+
+// Función para mostrar la notificación
+function mostrarNotificacion(mensaje, tipo) {
+    const notificacion = document.getElementById('notification');
+    const notificacionText = document.getElementById('notificationText');
+
+    notificacionText.textContent = mensaje;
+    notificacion.classList.remove('error'); // Remover clase de error (en caso de que esté presente)
+
+    if (tipo === 'error') {
+        notificacion.classList.add('error');
+    }
+
+    notificacion.style.display = 'block';
+
+    // Desaparecer la notificación después de 3 segundos
+    setTimeout(() => {
+        notificacion.style.display = 'none';
+    }, 3000);
+}
+
+
+
+function obtenerVariablesPHP() {
+  fetch('layout/getPHPVariables.php')
+    .then(response => response.json())
+    .then(data => {
+      // Aquí obtienes los nuevos valores de las variables PHP en el objeto "data"
+      // Puedes acceder a los valores como data.mensaje y data.error
+      // Por ejemplo:
+      var nuevoMensaje = data.mensaje;
+      var nuevoError = data.error;
+
+    
+
+      if(nuevoError==="true"){
+        
+        var re="success";
+        
+      }
+      if(nuevoError==="false"){
+      
+        var re="error";
+        
+      }
+
+      mostrarNotificacion(nuevoMensaje, re);
+     
+
+    })
+    .catch(error => {
+      console.error('Error al obtener las variables PHP:', error);
+    });
+}
+</script>
+
+<style>
+		/* Estilos para la notificación */
+.notification {
+    position: fixed;
+    top: 20px;
+    right: 20px;
+    background-color: #4CAF50;
+    color: #fff;
+    padding: 10px;
+    border-radius: 5px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+    display: none;
+    z-index: 9999;
+}
+.notification.error {
+    background-color: #f44336;
+}
+
+	</style>
+  
