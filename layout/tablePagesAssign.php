9c95ecc53adb9f88ff4e0b0d52811adb68ca7588
Juan <juan.paez@api-ux.com>
2023-08-04 11:07:31 -0500

    calculos por transmision v1.3.0-beta

--- layout/tablePagesAssign.php
+++ layout/tablePagesAssign.php
@@ -135,18 +135,20 @@ function asignarPagina(button, pageid, profileid) {
 
 
 
-
-
 // Función para mostrar la notificación
 function mostrarNotificacion(mensaje, tipo) {
-    const notificacion = document.createElement('div');
-    notificacion.classList.add('notification');
+    const notificacion = document.getElementById('notification');
+    const notificacionText = document.getElementById('notificationText');
+
+    notificacionText.textContent = mensaje;
+    notificacion.classList.remove('error'); // Remover clase de error (en caso de que esté presente)
+
     if (tipo === 'error') {
         notificacion.classList.add('error');
     }
-    notificacion.textContent = mensaje;
-    document.body.appendChild(notificacion);
-    
+
+    notificacion.style.display = 'block';
+
     // Desaparecer la notificación después de 3 segundos
     setTimeout(() => {
         notificacion.style.display = 'none';
