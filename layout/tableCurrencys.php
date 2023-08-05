13bd48a619e3dbf4a67dfa95b2f496e5365de7d7
Juan <juan.paez@api-ux.com>
2023-08-04 22:38:09 -0500

    sistemas embebidos v1.4.0-beta

--- layout/tableCurrencys.php
+++ layout/tableCurrencys.php
@@ -1,6 +1,7 @@
 
 
-
+<button onclick="descargarCSVcur()" class="table-button">Descargar resporte CSV</button>
+<button onclick="descargarExcelcur()" class="table-button">Descargar reporte Excel</button>
 <div class="table-container">
     
 <table id="currency-table" class="table">
@@ -96,4 +97,119 @@ $headerslink=$_SESSION['ranCode']." ".$_SESSION['key'];
           </table>
        
 </div>
+
+
+<script>
+function convertirTablaAFormatoCSVcur() {
+    const tabla = document.getElementById('currency-table');
+    const filas = tabla.getElementsByTagName('tr');
+    const contenidoCSV = [];
+
+    for (let i = 0; i < filas.length; i++) {
+        const celdas = filas[i].getElementsByTagName('td');
+        const filaCSV = [];
+
+        for (let j = 0; j < celdas.length; j++) {
+            filaCSV.push(celdas[j].innerText);
+        }
+
+        contenidoCSV.push(filaCSV.join(','));
+    }
+
+    return contenidoCSV.join('\n');
+}
+
+function descargarCSVcur() {
+    const contenidoCSV = convertirTablaAFormatoCSVcur();
+    const blob = new Blob([contenidoCSV], { type: 'text/csv' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+
+
+
+    const fecha = new Date();
+    const year = fecha.getFullYear();
+    const month = String(fecha.getMonth() + 1).padStart(2, '0'); // Sumamos 1 al mes ya que los meses en JavaScript van de 0 a 11
+    const day = String(fecha.getDate()).padStart(2, '0');
+    const fechaActual = `${year}-${month}-${day}`;
+    a.href = url;
+    a.download = 'reporte_monedas_creadas_'+fechaActual+'.csv';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+
+function convertirTablaAFormatoJSONcur() {
+    const tabla = document.getElementById('currency-table');
+    const filas = tabla.getElementsByTagName('tr');
+    const contenidoJSON = [];
+
+    for (let i = 1; i < filas.length; i++) {
+        const celdas = filas[i].getElementsByTagName('td');
+        const filaJSON = {};
+
+        for (let j = 0; j < celdas.length; j++) {
+            const columna = document.querySelector('th:nth-child(' + (j + 1) + ')').innerText;
+            filaJSON[columna] = celdas[j].innerText;
+        }
+
+        contenidoJSON.push(filaJSON);
+    }
+
+    return JSON.stringify(contenidoJSON, null, 2);
+}
+
+function descargarJSONcur() {
+    const contenidoJSON = convertirTablaAFormatoJSONcur();
+    const blob = new Blob([contenidoJSON], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+
+    a.href = url;
+    a.download = 'tabla.json';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+
+
+
+
+function convertirTablaAFormatoExcelcur() {
+    const tabla = document.getElementById('currency-table');
+    const filas = tabla.getElementsByTagName('tr');
+    const contenidoExcel = [];
+
+    for (let i = 0; i < filas.length; i++) {
+        const celdas = filas[i].getElementsByTagName('td');
+        const filaExcel = [];
+
+        for (let j = 0; j < celdas.length; j++) {
+            filaExcel.push(celdas[j].innerText);
+        }
+
+        contenidoExcel.push(filaExcel.join('\t'));
+    }
+
+    return contenidoExcel.join('\n');
+}
+
+function descargarExcelcur() {
+    const contenidoExcel = convertirTablaAFormatoExcelcur();
+    const blob = new Blob(['\ufeff', contenidoExcel], { type: 'application/vnd.ms-excel' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    const fecha = new Date();
+    const year = fecha.getFullYear();
+    const month = String(fecha.getMonth() + 1).padStart(2, '0'); // Sumamos 1 al mes ya que los meses en JavaScript van de 0 a 11
+    const day = String(fecha.getDate()).padStart(2, '0');
+    const fechaActual = `${year}-${month}-${day}`;
+    a.href = url;
+    a.download = 'reporte_monedas_creadas_'+fechaActual+'.xls';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+
+</script>
 
