0245686cabb4095006097d23fc36efe56cb931b3
Juan <juan.paez@api-ux.com>
2023-08-06 15:50:13 -0500

    sistemas embebidos v1.4.0-beta

--- layout/headerSuperAdmin.php
+++ layout/headerSuperAdmin.php
@@ -41,7 +41,7 @@
             <li class="navbar-item"><a href="#" onclick="openModalSchedule();getSch();">Mi Horario</a></li>
             <li class="navbar-item"><a href="#" onclick="openModalUsersSchedule();getUsersSuperAdminSc();">Horarios Generales</a></li>
             <li class="navbar-item"><a href="#" onclick="openModalUsersAlerts();getUsersSuperAdminAlerts();">Alertas generales</a></li>
-            <li class="navbar-item"><a href="#" onclick="openModalembed();">Tasa de cambio</a></li>
+            
             <li class="navbar-item"><a href="#" onclick="openModalembedcal();">Calculadora</a></li>
               </ul>
         </li>
