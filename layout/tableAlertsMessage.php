518efebb007c20f445bd20ccc66455b862720c23
Juan <juan.paez@api-ux.com>
2023-08-07 12:24:08 -0500

    sistemas base v1.0.0-pr

--- layout/tableAlertsMessage.php
+++ layout/tableAlertsMessage.php
@@ -11,7 +11,16 @@
 <div id="publicgroups-container"></div>
 
 
+<p id="valorVariable"></p>
 
+    <script>
+        // Obtener el valor de la variable almacenada en sessionStorage
+        const valor = sessionStorage.getItem('alertsCounter');
+
+        // Mostrar el valor en el documento HTML
+        const elementoValor = document.getElementById('valorVariable');
+        elementoValor.textContent = valor;
+    </script>
 
 
 
