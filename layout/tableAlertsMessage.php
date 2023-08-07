808a23ab304e64c2d62f565dc1c5610c80260877
Juan <juan.paez@api-ux.com>
2023-08-07 10:10:52 -0500

    sistemas base v1.0.0-pr

--- layout/tableAlertsMessage.php
+++ layout/tableAlertsMessage.php
@@ -6,14 +6,7 @@
 
 <h2>verifique el módulo de notificaciones</h2>
 <audio id="audioPlayer" src="public/alerts.mp3" preload="auto"></audio>
-<script>reproducirSonido();</script>
 
-<script>
-  function reproducirSonido() {
-    var audio = document.getElementById('audioPlayer');
-    audio.play();
-  }
-</script>
 
 
 <div id="publicgroups-container"></div>
