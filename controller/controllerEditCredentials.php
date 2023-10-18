8a2ad678a93f5501b05dc6b60046135a94e09f8a
Juan <juan.paez@api-ux.com>
2023-10-18 14:01:37 -0500

    pr base de prueba

---
+++ controller/controllerEditCredentials.php
@@ -0,0 +1,72 @@
+<?php
+
+session_start();
+$profileId = $_GET['profileId'];
+$pageid = $_GET['pageId'];
+$transId = $_GET['transId'];
+$param = $_GET['param'];
+$value = $_GET['value'];
+
+require_once '../env/domain.php';
+$sub_domaincon = new model_dom();
+$sub_domain = $sub_domaincon->dom();
+
+$url = $sub_domain . "/crystalGateway/apiIntegrations/v1/editCredentials/".$_SESSION['ranCode']."/".$_SESSION['key'];
+
+// Definir los datos a enviar en la solicitud POST
+$data = array(
+    
+    'transId' => $transId,
+    'pageId' => $pageid,
+    'profileId' => $profileId,
+    'param' => $param,
+    'value' => $value
+    
+);
+
+// Convertir los datos a formato JSON
+$json_data = json_encode($data);
+
+// Inicializar la sesión cURL
+$curl = curl_init();
+
+// Configurar las opciones de la sesión cURL
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+// Ejecutar la solicitud y obtener la respuesta
+$response1 = curl_exec($curl);
+
+// Cerrar la sesión cURL
+curl_close($curl);
+
+//$response1 = trim($response1); // Eliminar espacios en blanco alrededor de la respuesta
+$array = explode("*", $response1);
+$response12=$array[0];
+$message=$array[1];
+//echo $_SESSION['key'];
+
+$response1 = trim($response12); // Eliminar espacios en blanco alrededor de la respuesta
+
+if (strtolower($response1) === "true") { // Convertir la respuesta a minúsculas antes de comparar
+  
+    $_SESSION["respuesta"] = $response1;
+    $_SESSION["mensaje"] = $message;
+    $_SESSION["error"] = "true";
+    
+   // header ('Location: ../room.php?roomId='.$roomId);
+}
+
+if (strtolower($response1) != "true") { // Convertir la respuesta a minúsculas antes de comparar
+    
+    $_SESSION["respuesta"] = $response1;
+    $_SESSION["mensaje"] = $message;
+    $_SESSION["error"] = "true";
+  
+  
+  
+    //header ('Location: ../room.php?roomId='.$roomId);
+}
+?>
--- layout/tablePagesListModelsNot.php
+++ layout/tablePagesListModelsNot.php
@@ -52,8 +52,8 @@ $headerslink=$_SESSION['ranCode']." ".$_SESSION['key'];
       <td>${student.pageName}</td>
         <td>${student.urlPage}</td>
         <td>${student.isActive}</td>
-        <td>${student.pageUserName}</td>
-        <td>${student.pageKey}</td>
+        <td><input type="text" id="pageuname" name="pageuname" placeholder="Ingresa nombre de usuario" value="${student.pageUserName}"><button onclick="editarCredenciales(this,&quot;${student.pageId}&quot;,&quot;${student.modelId}&quot;,&quot;${student.transId}&quot;,&quot;pName&quot;);" class="table-button">Editar</button></td>
+        <td><input type="text" id="pageKey" name="pagekey" placeholder="Ingresa contraseña" value="${student.pageKey}"><button onclick="editarCredenciales(this,&quot;${student.pageId}&quot;,&quot;${student.modelId}&quot;,&quot;${student.transId}&quot;,&quot;pKey&quot;);" class="table-button">Editar</button></td>
        
 
         
@@ -222,4 +222,34 @@ function conectarTransmisionModel(button, pageid, profileid,transid) {
       console.log('Error en la petición:', error);
     });
 }
+
+
+
+
+
+function editarCredenciales(button, pageid, profileid,transid,param) {
+  // Obtener el valor del campo de texto correspondiente al botón
+  var input = button.previousElementSibling;
+  var nombre = input.value;
+
+  // Construir la URL con los parámetros de la petición GET
+  var url = 'controller/controllerEditCredentials.php?pageId=' + encodeURIComponent(pageid) + '&profileId=' + encodeURIComponent(profileid) + '&transId=' + encodeURIComponent(transid) + '&param=' + encodeURIComponent(param)+ '&value=' + encodeURIComponent(nombre);
+
+  // Realizar la petición GET al archivo PHP
+  fetch(url)
+    .then(response => {
+      // Aquí puedes realizar alguna acción con la respuesta del servidor, si lo deseas
+      // Por ejemplo, mostrar un mensaje de éxito o actualizar la información en la página
+      
+      //getSch();
+      obtenerVariablesPHP();
+      getPagesAssignModelsNot();
+      
+    })
+    .catch(error => {
+      // Aquí puedes manejar los errores en caso de que la petición falle
+      console.log('Error en la petición:', error);
+    });
+}
+
 </script>
